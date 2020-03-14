c69459ca3d00da6f287c20ddb13fc3aaec110ff8
George <george@fozzie.dev>
2020-03-14 21:12:21 +0000

    Add confirmation to run script

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 import glob, os
 def main():
-	for fn in glob.glob("*.RANSOM"):
-		os.rename(fn, fn + '.exe')
-		print("Renaming " + fn + " to " + fn + ".exe")
-	for fn in glob.glob("*.exe"):
-		os.startfile(fn)
-		print("Running " + fn)
+	start = bool(input("This will run all of the ransomware in this folder! If you are not in a safe environment do not run this script.\nAre you ready to proceed? (yes/no)")
+	if start:
+		for fn in glob.glob("*.RANSOM"):
+			os.rename(fn, fn + '.exe')
+			print("Renaming " + fn + " to " + fn + ".exe")
+		for fn in glob.glob("*.exe"):
+			os.startfile(fn)
+			print("Running " + fn)
+	else:
+		return
 main();
