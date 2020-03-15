102acce81323b4feee344306121399a6b33d6147
George <george@fozzie.dev>
2020-03-15 14:33:36 +0000

    Better handle AVs blocking the file renames

--- run.py
+++ run.py
@@ -6,28 +6,48 @@ def main():
 	current = 0
 	
 	if start == "yes":
+		print("--------------------------------------------------------------")
+		total = len(glob.glob("*.RANSOM"))
+		print("Renaming " + str(total) + " samples (in some cases the AV will block renames here).")
 		for fn in glob.glob("*.RANSOM"):
-			print("Renaming " + fn + " to " + fn + ".exe")
-			os.rename(fn, fn + '.exe')
-			
-		total = len(glob.glob("*.exe"))
-		print("Running " + str(total) + " samples.")
-		for fn in glob.glob("*.exe"):
 			try:
 				current += 1
-				p = subprocess.Popen([fn])
-				result = p.wait()
-				if result == 1:
-					missed += 1
-					print("(" + str(current) + "/" + str(total) + ") " + fn + " missed (" + str(round(((total - missed)/total) * 100, 2)) + "%)")
-				else:
+				os.rename(fn, fn + '.exe')
+				print("Renamed " + fn + " to " + fn + ".exe")
+			except:
+				blocked += 1
+				print("Rename (" + str(current) + "/" + str(total) + ") " + fn + " blocked (" + str(round(((total - missed)/total) * 100, 2)) + "%)")
+				pass
+		print("--------------------------------------------------------------")
+		if (len(glob.glob("*.exe")) == 0):
+			print("No executables to run, they've been deleted!")
+			print("--------------------------------------------------------------")
+			print("Results:\n    Blocked: " + str(blocked) + "\n    Missed: " + str(missed) + "\n    Detection Score: " + str(round(((total - missed)/total) * 100, 2)) + "%.")
+			print("--------------------------------------------------------------")
+		else:
+			total = len(glob.glob("*.exe"))
+			blocked = 0
+			missed = 0
+			current = 0
+			print("Running " + str(total) + " samples.")
+			for fn in glob.glob("*.exe"):
+				try:
+					current += 1
+					p = subprocess.Popen([fn])
+					result = p.wait()
+					if result == 1:
+						missed += 1
+						print("(" + str(current) + "/" + str(total) + ") " + fn + " missed (" + str(round(((total - missed)/total) * 100, 2)) + "%)")
+					else:
+						blocked += 1
+						print("(" + str(current) + "/" + str(total) + ") " + fn + " blocked (" + str(round(((total - missed)/total) * 100, 2)) + "%)")
+				except:
 					blocked += 1
 					print("(" + str(current) + "/" + str(total) + ") " + fn + " blocked (" + str(round(((total - missed)/total) * 100, 2)) + "%)")
-			except:
-				print("(" + str(current) + "/" + str(total) + ") " + fn + " blocked (" + str(round(((total - missed)/total) * 100, 2)) + "%)")
-				blocked += 1
-				pass
-		print("\n\nResults:\n    Blocked: " + str(blocked) + "\n    Missed: " + str(missed) + "\n    Detection Score: " + str(round(((total - missed)/total) * 100, 2)) + "%.")
+					pass
+			print("--------------------------------------------------------------")
+			print("Results:\n    Blocked: " + str(blocked) + "\n    Missed: " + str(missed) + "\n    Detection Score: " + str(round(((total - missed)/total) * 100, 2)) + "%.")
+			print("--------------------------------------------------------------")
 	else:
 		return
 main();
